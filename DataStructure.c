1740bdd7c5f7504970aa86c110857f27288d17f0
Hyunju Yang <sw4019@naver.com>
2021-11-20 14:05:21 +0000

    Initial commit

---
+++ DataStructure.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DataStructure.h"
+
+/*DBLinkedList*/
+void initDBLinkedList(DBLinkedList* plist) {
+	plist->head = NULL;
+	plist->length = 0;
+}
+
+void insertDBLinkedList(DBLinkedList* plist, ListData data) {
+	DoubleNode* nNode = (DoubleNode*)malloc(sizeof(DoubleNode));
+	nNode->data = data;
+	nNode->next = plist->head;
+	if(plist->head != NULL) {
+		plist->head->prev = nNode;
+	}
+	nNode->prev = NULL;
+	plist->head=nNode;
+	(plist->length)++;
+}
+
+int getFirstDBLinkedList(DBLinkedList* plist, ListData* data) {
+	if(plist->head == NULL) return FALSE;
+	plist->cur = plist->head;
+	*data = plist->cur->data;
+	return TRUE;
+}
+int getNextDBLinkedList(DBLinkedList* plist, ListData* data) {
+	if(plist->cur->next == NULL) return FALSE;
+	plist->cur = plist->cur->next;
+	*data = plist->cur->data;
+	return TRUE;
+}
+int getPreviousDBLinkedList(DBLinkedList* plist, ListData* data) {
+	if(plist->cur->prev == NULL) return FALSE;
+	plist->cur = plist->cur->prev;
+	*data = plist->cur->data;
+	return TRUE;
+}
+
+int getDBLinkedListLength(DBLinkedList* plist, ListData* data) {return plist->length;}
+
+/*Binayr Tree*/
+BTreeNode* MakeBTreeNode(void) {
+	BTreeNode* nd = (BTreeNode*)malloc(sizeof(BTreeNode));
+
+	nd->left = NULL;
+	nd->right = NULL;
+	return nd;
+}
+
+BTData GetBTData(BTreeNode* bt) {
+	return bt->data;
+}
+
+void SetBTData(BTreeNode* bt,BTData data) {
+	bt->data = data;
+}
+
+BTreeNode* GetBTLeftSubTree(BTreeNode* bt) {
+	return bt->left;
+}
+
+BTreeNode* GetBTRightSubTree(BTreeNode* bt) {
+	return bt->right;
+}
+
+void MakeBTLeftSubTree(BTreeNode* main, BTreeNode* sub) {
+	if(main->left != NULL) {
+		free(main->left);
+	}
+	main->left = sub;
+}
+
+void MakeBTRightSubTree(BTreeNode* main, BTreeNode* sub) {
+	if(main->right != NULL) {
+		free(main->right);
+	}
+	main->right = sub;
+}
+
+void inorderTraverseBT(BTreeNode* bt) {
+	if(bt==NULL) return;
+	printf("%d ",bt->data);
+	inorderTraverseBT(bt->left);
+	inorderTraverseBT(bt->right);
+}
---
+++ DataStructure.h
@@ -0,0 +1,95 @@
+
+
+#define TRUE 1
+#define FALSE 0
+
+/* Default Data Type */
+typedef int ListData;
+typedef int BTData;
+
+
+
+typedef struct __doubleNode {
+	ListData data;
+	struct __doubleNode* prev;
+	struct __doubleNode* next;
+} DoubleNode;
+
+typedef struct __doubleNodeLinkedList {
+	DoubleNode* head;
+	DoubleNode* cur;
+	int length;
+} DBLinkedList;
+
+typedef struct __bTreeNode{
+	BTData data;
+	struct __bTreeNode *left;
+	struct __bTreeNode *right;
+} BTreeNode;
+
+/*DBLinkedList Func */
+
+void initDBLinkedList(DBLinkedList* plist);
+
+void insertDBLinkedList(DBLinkedList* plist, ListData data);
+
+int getFirstDBLinkedList(DBLinkedList* plist, ListData* data);
+int getNextDBLinkedList(DBLinkedList* plist, ListData* data);
+int getPreviousDBLinkedList(DBLinkedList* plist, ListData* data);
+
+int getDBLinkedListLength(DBLinkedList* plist, ListData* data);
+
+/* Binary Tree Func */
+
+BTreeNode* MakeBTreeNode(void);
+
+BTData GetBTData(BTreeNode * bt);
+
+void SetBTData(BTreeNode* bt, BTData data);
+
+BTreeNode* GetBTLeftSubTree(BTreeNode* bt);
+
+BTreeNode* GetBTRightSubTree(BTreeNode* bt);
+
+void MakeBTLeftSubTree(BTreeNode* main, BTreeNode* sub);
+
+void MakeBTRightSubTree(BTreeNode* main, BTreeNode *sub);
+
+void inorderTraverseBT(BTreeNode* bt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
