57acd21e81677099426f6d8ff18f94f248c27291
Hyunju Yang <sw4019@naver.com>
2021-12-03 00:23:36 +0000

    ㄴㅁㅇㄹㄴㅇㅁㄹㄴㅁㄹ

--- main.c
+++ main.c
@@ -131,34 +131,5 @@ int main(void) {
 		}
 
 	}
-	/*
-	printf("양방향 연결 리스트\n");
-	DBLinkedList dlist;
-	ListData data; //int
-	initDBLinkedList(&dlist);
-	insertDBLinkedList(&dlist, 1);
-	insertDBLinkedList(&dlist, 2);
-	insertDBLinkedList(&dlist, 3);
-	insertDBLinkedList(&dlist, 4);
-	insertDBLinkedList(&dlist, 5);
-	insertDBLinkedList(&dlist, 6);
-	
-
-	
-	printf("이진 트리\n");
-	BTreeNode* bt1 = MakeBTreeNode();
-	BTreeNode* bt2 = MakeBTreeNode();
-	BTreeNode* bt3 = MakeBTreeNode();
-
-	SetBTData(bt1, 1);
-	SetBTData(bt2, 2);
-	SetBTData(bt3, 3);
-
-	MakeBTLeftSubTree(bt1, bt2);
-	MakeBTRightSubTree(bt1, bt3);
-
-	inorderTraverseBT(bt1);
-	printf("\n");
-	*/
 
 }
